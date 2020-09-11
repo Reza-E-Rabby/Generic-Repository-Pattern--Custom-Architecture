a8feb8c381530381966e6b2432dd101e146fa82b
Md. Reza-E Rabby <reza.rabby@pakizatvl.com>
2020-09-11 20:32:34 +0600

    fist

---
+++ Queries/Program.cs
@@ -0,0 +1,33 @@
+﻿using Queries.Core.Domain;
+using Queries.Persistence;
+using System;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var unitOfWork = new UnitOfWork(new PlutoContext()))
+            {
+                // Example1
+                Course obj= new Course(); //Name,Description,Level,FullPrice,Author,AuthorId,Tags,Cover,IsBeginnerCourse
+                obj.Name = "Rez";
+                obj.Description = "Reza";
+                obj.Level = 1;
+                obj.FullPrice = 100;
+                obj.AuthorId = 25;
+                string QueryString = unitOfWork.Courses.GetQueryString(obj);
+
+                Author objA = new Author();
+                string QueryStringAut = unitOfWork.Authors.GetQueryString(objA);
+
+                Console.WriteLine("Course Insert Query : " + QueryString);
+                //Console.WriteLine("Author Insert Query : " + QueryStringAut);
+                // Example2
+                
+            }
+        }
+    }
+}
+  
